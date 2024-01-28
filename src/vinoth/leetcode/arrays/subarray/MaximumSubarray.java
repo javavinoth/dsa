a61f1df74b432233c90826710a6d2528c9fca024
javavinoth <vinoth.sakthivel95@gmail.com>
2024-01-28 13:25:55 +0530

    refactored

--- src/vinoth/leetcode/arrays/subarray/MaximumSubarray.java
+++ src/vinoth/leetcode/arrays/subarray/MaximumSubarray.java
@@ -8,21 +8,19 @@ public class MaximumSubarray {
     public static int subArraySumBruteForce(int[] arr) {
         int max = Integer.MIN_VALUE;
         for (int start = 0; start < arr.length; start++) {
-            for (int end = 0; end < arr.length; end++) {
+            for (int end = start; end < arr.length; end++) {
                 int sum = 0;
                 for (int k = start; k <= end; k++) {
                     sum = sum + arr[k];
                 }
-                if (max < sum) {
-                    max = sum;
-                }
+                max = Math.max(max, sum);
             }
         }
         return max;
     }
 
     public static void main(String[] args) {
-        int[] arr = {5, 4, -1, 7, 8};
+        int[] arr = {-2, -1};
         System.out.println(subArraySumBruteForce(arr));
     }
 
